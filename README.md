520426d2e7d9558fb8bd605ce60cf56717d67350
Alixe Langshall <frankblues@gmail.com>
2024-06-21 07:18:04 -0700

    It's a quickand dirty google chat parser

---
+++ README.md
@@ -0,0 +1,12 @@
+# Quick and dirty google chat takeout parser
+
+This program takes the input of a google chat takeout .json file and returns text.
+
+### Usage
+gchat-parse.py [-h] filename
+
+positional arguments:
+  filename    JSON file from takeout of Google Chat
+
+optional arguments:
+  -h, --help  show this help message and exit
---
+++ gchat-parse.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+import argparse
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="JSON file from takeout of Google Chat", type=str)
+args = parser.parse_args() 
+
+f = open(args.filename)
+ 
+data = json.load(f)
+ 
+for message in data["messages"]:
+    if 'created_date' not in message:
+        if 'text' not in message:
+            print(
+            f"""{message["creator"]["name"]}\n"""
+            )
+        else:
+            print(
+                f"""{message["creator"]["name"]}\n{message["text"]}\n"""
+            )
+    else: 
+        if 'text' not in message:
+            print(
+            f"""{message["created_date"]}\n{message["creator"]["name"]}\n"""
+            )
+        else:
+            print(
+            f"""{message["created_date"]}\n{message["creator"]["name"]}\n{message["text"]}\n"""
+        )
+f.close()
